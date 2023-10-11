3f6ad1e41380d648d4981a62a3e3d171e8ecd604
t.arnaudova <tedy_arnaudova@abv.bg>
2023-10-11 13:12:29 +0300

    some changes

--- src/controllingExecution/Numbers1To100.java
+++ src/controllingExecution/Numbers1To100.java
@@ -4,6 +4,9 @@ public class Numbers1To100 {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100 ; i++) {
+            if (i == 99){
+              return;
+            }
             System.out.println(i);
         }
     }
